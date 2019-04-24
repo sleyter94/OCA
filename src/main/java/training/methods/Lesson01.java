e81e2cbd6bd78a995e4f055cdca8fdd65ebde960
Sleyter Sandoval <ssandoval@epcmexperts.com>
2019-04-24 16:59:50 -0500

    Changes for lesson 1

--- src/main/java/training/methods/Lesson01.java
+++ src/main/java/training/methods/Lesson01.java
@@ -3,7 +3,27 @@ package training.methods;
 public class Lesson01 {
 
 	public static void main(String[] args) {
-
+			
 	}
 
+	
+	
+}
+
+class B {
+	void y(){
+		this.z();
+	}
+	static void z(){
+		System.out.println("z");
+	}
+}
+
+class A {
+	public static void  main(String[] args){
+		new A().x();
+	}
+	static void x(){
+		new B().y();
+	}
 }
